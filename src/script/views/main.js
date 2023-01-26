9585a3b004be86bae89c05328010e12cb30460a2
Hawari Muflih Munte <90821837+hawariMuflihMunte@users.noreply.github.com>
2023-01-26 16:27:40 +0700

    Implementing seach function and delete dummy data

--- src/script/views/main.js
+++ src/script/views/main.js
@@ -3,14 +3,12 @@ import "../components/nav-search";
 function main() {
     const apiKey = "30d1ab19";
     const baseUrl = `http://omdbapi.com/?apikey=${apiKey}&`;
-    // const keyword = document.querySelector("nav-search").value;
-    const keyword = "cars";
+    const _search = document.querySelector("nav-search");
 
     const _movie = [];
-    const movieContainer = document.getElementById("list-item");
 
-
-    return fetch(`${baseUrl}&s=${keyword}`)
+    const search = (keyword) => {
+        return fetch(`${baseUrl}&s=${keyword}`)
         .then(response => {
             return response.json();
         })
@@ -21,76 +19,49 @@ function main() {
             else {
                 return Promise.reject(`${keyword} tidak ditemukan`);
             }
-        })
-        .then(json => {
-            _movie.push(json);
+        });
+    };
 
-            // console.log(_movie[0]);
+    const renderResult = (obj) => {
+        const movieContainer = document.getElementById("list-item");
+        movieContainer.innerHTML = "";
 
-            for (const m of _movie[0].Search) {
-                const _list = document.createElement("div");
-                _list.innerHTML = `
-                    <div class="uk-card uk-card-default">
-                        <div class="uk-card-media-top">
-                            <img src="${m.Poster}" alt="${m.Title}'s poster" width="1800" height="1200">
-                        </div>
-                        <div class="uk-card-body">
-                            <h3 class="uk-card-title"></h3>
-                            <ul class="uk-list uk-list-striped">
-                                <li class="uk-flex uk-flex-between"><span>Title:</span>${m.Title}</li>
-                                <li class="uk-flex uk-flex-between"><span>Year:</span>${m.Year}</li>
-                                <li class="uk-flex uk-flex-between"><span>Type:</span>${m.Type}</li>
-                            </ul>
-                            <div class="uk-flex uk-flex-center">
-                                <button class="uk-button uk-button-primary">details</button>
-                            </div>
+        for (const m of obj) {
+            const _list = document.createElement("div");
+            _list.innerHTML = `
+                <div class="uk-card uk-card-default">
+                    <div class="uk-card-media-top">
+                        <img src="${m.Poster}" alt="${m.Title}'s poster" width="1800" height="1200">
+                    </div>
+                    <div class="uk-card-body">
+                        <h2 class="uk-card-title uk-margin-remove">${m.Title}</h2>
+                        <h4 class="uk-text-muted uk-margin-remove">${m.Year}</h4>
+                        <h5 class="uk-text-muted uk-margin-remove uk-margin-small-bottom uk-text-right uk-text-uppercase">${m.Type}</h5>
+                        <hr>
+                        <div class="uk-flex uk-flex-center">
+                            <button class="uk-button uk-button-primary">details</button>
                         </div>
                     </div>
-                `;
-                movieContainer.appendChild(_list);
-            }
-        })
-        .catch(error => {
-            console.log(error);
-        });
-    
-    // fetch("http://omdbapi.com/?apikey=30d1ab19&s=jujutsu")
-    // .then(response => {
-    //     if (response.ok) {
-    //         return response.json();
-    //     }
-    //     else {
-    //         return Promise.reject({
-    //             status: response.status
-    //         });
-    //     }
-    // })
-    // .then(json => {
-    //     _movie.push(json);
-    
-    //     console.log(_movie[0].Search);
-    
-    //     for (const m of _movie[0].Search) {
-    //         const _list = document.createElement("div");
-    //         _list.innerHTML = `
-    //             <div class="uk-card uk-card-default">
-    //                 <div class="uk-card-media-top">
-    //                     <img src="${m.Poster}" alt="${m.Title}'s poster" width="1800" height="1200">
-    //                 </div>
-    //                 <div class="uk-card-body">
-    //                     <h3 class="uk-card-title">${m.Title}</h3>
-    //                     <p>
-                            
-    //                     </p>
-    //                 </div>
-    //             </div>
-    //         `;
-    //         movieContainer.appendChild(_list);
-    //     }
-    // })
-    // .catch(error => {
-    //     console.log(error.status);
-    // });
+                </div>
+            `;
+
+            movieContainer.appendChild(_list);
+        }
+    }
+
+    const searchResult = async () => {
+        try {
+            const keyword = _search.value;
+            const result = await search(keyword);
+
+            renderResult(result.Search);
+            console.log(result.Search[0]);
+        } catch (error) {
+            window.alert(error);
+        }
+    };
+
+    _search.click = searchResult;
 }
 
 export default main;
